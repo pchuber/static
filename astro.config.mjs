f8dc0236cb1c830005a5fd3662f9e01eb7324399
pchuber <96705871+pchuber@users.noreply.github.com>
2026-08-05 19:30:47 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://username.github.io',
-  base: '/',
+  base: '/static',
   image: {
     service: { id: 'passthrough' },
   },
